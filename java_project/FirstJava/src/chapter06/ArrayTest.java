d6086f98b4d9ad44eda1a12b501ae10a8a4827f5
hyunsooHi <baeaddbgg@naver.com>
2021-10-15 11:03:51 +0900

    2021.10.15

--- java_project/FirstJava/src/chapter06/ArrayTest.java
+++ java_project/FirstJava/src/chapter06/ArrayTest.java
@@ -4,11 +4,97 @@ public class ArrayTest {
 
 	public static void main(String[] args) {
 		
+		int[][] arr1= {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9},
+				{10,11,12}
+		};
+		//출력
+		for(int i=0;i<arr1.length;i++) {
+			for(int j=0;j<arr1[i].length;j++) {
+				System.out.print(arr1[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		System.out.println("=====================");
+		//////////////////////////////
+		//행의 시프트
+		
+		//임시배열선언
+		int[] temp = arr1[arr1.length-1];
+		
+		for(int i=arr1.length-1;i>0;i--) {
+			arr1[i] = arr1[i-1];
+			
+		}
+		arr1[0] = temp;
+		//////////////////////////////////////
+		
+		//출력
+		for(int i=0;i<arr1.length;i++) {
+			for(int j=0;j<arr1[i].length;j++) {
+				System.out.print(arr1[i][j]+"\t");
+			}
+			System.out.println();
+		}
+				
+		
+		System.out.println("=====================");
+		
 		int[] arr= {100,22,55};
 		
 		System.out.println("최대: "+maxValue(arr));
 
-		System.out.println("최대: "+miniValue(arr));
+		System.out.println("최소: "+miniValue(arr));
+		
+		//2차원 배열의 선언
+		int[][] arr3 = {
+				{1,2,3},
+				{4,5,6,7},
+				{8,9,10,11,12,13}
+				
+		};
+		
+		System.out.println("2차원배열출력");
+		addOneDArr(arr3, 10);
+		
+	}
+	
+	
+	
+	
+	public static void addOneDArr(int[][] arr, int add) {
+		
+		//2차원 배열의출력
+		//행
+		for(int i=0;i<arr.length;i++) {
+			//열
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		
+		//매개변수로 전달받은 숫자를 각 요소에 더해준다
+		for(int i=0;i<arr.length;i++) {
+			//열
+			for(int j=0;j<arr[i].length;j++) {
+				arr[i][j] += add;
+			}
+			
+		}
+		
+		System.out.println("각 요소에 숫자를 더하기====================");
+		
+		for(int i=0;i<arr.length;i++) {
+			//열
+			for(int j=0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		
 	}
 
 	public static int miniValue(int[] arr) {
