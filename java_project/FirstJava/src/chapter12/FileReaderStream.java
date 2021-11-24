736d604f6345241dd0e4b4bacfbf8324f0371447
hyunsooHi <baeaddbgg@naver.com>
2021-11-24 19:20:29 +0900

    1124

--- java_project/FirstJava/src/chapter12/FileReaderStream.java
+++ java_project/FirstJava/src/chapter12/FileReaderStream.java
@@ -8,16 +8,15 @@ import java.io.Reader;
 public class FileReaderStream {
 
 	public static void main(String[] args) throws IOException {
-
-		Reader in = new FileReader("hyper.txt");
+		Reader in = new FileReader("test.txt");
 
 		int readCnt = 0;
-		char[] cbuf = new char[10];
+		char[] cbuf = new char[100];
 
 		readCnt = in.read(cbuf, 0, cbuf.length);
 
 		for (int i = 0; i < readCnt; i++) {
-			System.out.println(cbuf[i]);
+			System.out.print(cbuf[i]);
 			
 		}
 		
--- java_project/FirstJava/src/review/ScoreManager.java
+++ java_project/FirstJava/src/review/ScoreManager.java
@@ -31,33 +31,33 @@ public class ScoreManager {
 		
 	}
 	// 데이터 검색 : 이름 기준 검색 -> 배열의 요소들 중 이름(검색 어)을 비교
-	public void search
-	
-	// 데이터 삭제 : 이름 기준으로 삭제 
-		// -> 배열에 요소에 손흥민 정보를 지운다! -> 손흥민 객체를 참조하는 값이 없으면 삭제
-	public void deleteScore(String name) {
-		search
-	}
-	
-	
-	// 이름으로 배열의 index 찾아 index 값을 반환하는 메소드
-	
-	// 학생 정보를 저장하고 -> 배열에 저장하는 메소드
-	
-	
-	public void index
-	
-	
-	public int searchIndex(String name) {
-		
-		for(int i = 0 ; i < numOfStudent ; i++) {
-			if (score[i].getName().equals(name)) {
-				return i;
-			}
-		}
-		
-	}
-	
+//	public void search
+//	
+//	// 데이터 삭제 : 이름 기준으로 삭제 
+//		// -> 배열에 요소에 손흥민 정보를 지운다! -> 손흥민 객체를 참조하는 값이 없으면 삭제
+//	public void deleteScore(String name) {
+//		search
+//	}
+//	
+//	
+//	// 이름으로 배열의 index 찾아 index 값을 반환하는 메소드
+//	
+//	// 학생 정보를 저장하고 -> 배열에 저장하는 메소드
+//	
+//	
+//	public void index
+//	
+//	
+//	public int searchIndex(String name) {
+//		
+//		for(int i = 0 ; i < numOfStudent ; i++) {
+//			if (score[i].getName().equals(name)) {
+//				return i;
+//			}
+//		}
+//		
+//	}
+//	
 	
 	
 	
